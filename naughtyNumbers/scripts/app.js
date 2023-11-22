1f396d8d500719e1e3907036f5842da6e6a2758f
BeLieVer031 <sandyrajak031@gmail.com>
2023-11-22 17:20:48 +0530

    logic building under process

--- naughtyNumbers/scripts/app.js
+++ naughtyNumbers/scripts/app.js
@@ -1,12 +1,25 @@
 const inp = document.querySelector("#input-data")
+const option1 = document.querySelector("#option1")
+const option2 = document.querySelector("#option2")
 const convertBtn = document.querySelector("#convert")
-// console.log(typeof inp);
-console.log(inp.value);
-console.log(convertBtn);
 
-
+// console.log(inp, option1, option2, convertBtn);
 
 convertBtn.addEventListener("click", function () {
-    console.log(this.innerHTML);
-})
+    // console.log(inp.value);
+    console.log(typeof +option1.value);
+    // console.log(option2.value);
 
+    // const decimalNum = parseInt("a", "octal") //10
+    // const number = decimalNum.toString(16);
+
+    // console.log(number);
+
+});
+
+const obj = {
+    name: "dilip",
+    value: "45"
+}
+
+// console.log(obj.value);
