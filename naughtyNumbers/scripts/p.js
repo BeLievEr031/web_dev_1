7d4f528ed3b1fdd38bea0c70d606ac9031a14711
BeLieVer031 <sandyrajak031@gmail.com>
2023-11-23 17:07:41 +0530

    p.js added

---
+++ naughtyNumbers/scripts/p.js
@@ -0,0 +1,9 @@
+const number = "81848"
+
+for (let i = 0; i < number.length; i++) {
+    if (number.charCodeAt(i) >= option1.value.charCodeAt(0)) {
+        inp.style.backgroundColor = "red";
+        return;
+    }
+}
+"10"
