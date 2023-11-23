a324569bd2c98c004747e02ca68e641755e4f571
BeLieVer031 <sandyrajak031@gmail.com>
2023-11-23 17:08:07 +0530

    half js done for app

--- naughtyNumbers/scripts/app.js
+++ naughtyNumbers/scripts/app.js
@@ -19,9 +19,44 @@ convertBtn.addEventListener("click", function () {
     const number = inp.value;
     const fromBase = +option1.value;
     const toBase = +option2.value
+
+    for (let i = 0; i < number.length; i++) {
+        // A ka ASCCI CODE NIKALO = 49
+        // SELECTED OPTION KE VALUE MEI SE - 11 fromBase-11
+        // A ASCII CODE MEI +PLUS 49 + 5 = 54
+        // HOW TO CONVERT ASCII VALUE INTO STRING
+
+        if (fromBase === 10) {
+            break;
+        }
+
+        if (fromBase >= 10) {
+
+            const asciiOfA = "A".charCodeAt(0) // ASCII CODE FOR A
+            const difference = fromBase - 11;
+            // 16 - 11 === 5
+            const newASCIICode = asciiOfA + difference;
+            const newBaseStr = String.fromCharCode(newASCIICode)
+
+            if (number.toUpperCase().charCodeAt(i) >= newBaseStr.charCodeAt(0)) {
+                inp.style.backgroundColor = "red";
+                return;
+            }
+
+        }
+
+        if (fromBase < 10 && number.charCodeAt(i) >= option1.value.charCodeAt(0)) {
+            inp.style.backgroundColor = "red";
+            return;
+        }
+
+
+
+
+    }
+
     const decimalNum = parseInt(number, fromBase)
     result.value = decimalNum.toString(toBase);
-
 });
 
 // console.log(obj.value);
