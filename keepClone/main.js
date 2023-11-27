15aee440615d1cd2291e281b1117f91da88528f2
BeLieVer031 <sandyrajak031@gmail.com>
2023-11-27 16:10:52 +0530

    Assignment is given

--- keepClone/main.js
+++ keepClone/main.js
@@ -1,11 +1,16 @@
 const menu = document.querySelector(".menu")
 const aside = document.querySelector("aside")
 let asideToggle = false;
+
+
+// 🔥🔥🔥 Replace the logo according to the active sidebar link.
+
+
 menu.addEventListener("click", () => {
 
-    if(asideToggle === false){
+    if (asideToggle === false) {
         aside.style.width = "10vh"
-    }else{
+    } else {
         aside.style.width = "40vh"
     }
 
