4fe0717cfeb837145db0e588d32023e292f8f04f
BeLieVer031 <sandyrajak031@gmail.com>
2023-11-28 17:46:20 +0530

    task input toggle algorithm discussed

--- keepClone/main.js
+++ keepClone/main.js
@@ -1,5 +1,6 @@
 const menu = document.querySelector(".menu")
 const aside = document.querySelector("aside")
+const taskTakerInp = document.querySelector(".add-task")
 let asideToggle = false;
 
 
@@ -17,3 +18,31 @@ menu.addEventListener("click", () => {
     asideToggle = !asideToggle
 })
 
+
+// aside.addEventListener("mousemove", (e) => {
+//     // console.log(e.target);
+//     const elem = Array.from(e.target.classList)
+//     console.log(elem);
+
+//     if (!asideToggle) {
+//         return;
+//     }
+
+//     if (elem.includes("items") || elem.includes("sidebar-icon-data")) {
+//         aside.style.width = "40vh"
+//     } else {
+//         aside.style.width = "10vh"
+
+//     }
+
+// })
+
+
+taskTakerInp.addEventListener("focus", function () {
+    // alert(45)
+    document.querySelector(".task-add-inp").style.height = "100px"
+})
+
+document.addEventListener("click",function(e){
+    console.log(e.target);
+})
