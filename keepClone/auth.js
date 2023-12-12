2f5a151bfd6b66d3d8724e2f5a8f2647819458ab
BeLieVer031 <sandyrajak031@gmail.com>
2023-12-12 18:05:03 +0530

    auth done

--- keepClone/auth.js
+++ keepClone/auth.js
@@ -1,16 +1,19 @@
-import { Client, Account } from "appwrite";
+import { Client, Account, ID } from "appwrite";
 // import { client } from "./app";
 
 let isUser = window.localStorage.getItem("user")
 let user = JSON.parse(isUser)
 
-if (user && user.email && user.userID) {
+if (user && user.email && user.$id) {
     window.location.replace("/");
 }
 
 const loginInp = document.querySelector("#login-inp")
 const loginPSW = document.querySelector("#login-psw")
 const loginBtn = document.querySelector("#login-btn")
+const signInp = document.querySelector("#sign-email")
+const signPSW = document.querySelector("#sign-pwd")
+const signBtn = document.querySelector("#sign-btn")
 
 
 const client = new Client();
@@ -23,17 +26,25 @@ client
 const account = new Account(client);
 
 
+signBtn.addEventListener("click", function (e) {
+    e.preventDefault()
+    const promise = account.create(ID.unique(), signInp.value, signPSW.value);
+
+    promise.then(function (response) {
+        document.querySelector("#login-chk").click()
+        loginInp.value = signInp.value;
+    }, function (error) {
+        console.log(error); // Failure
+    });
+})
 
 
 loginBtn.addEventListener("click", function (e) {
     e.preventDefault()
     const promise = account.createEmailSession(loginInp.value, loginPSW.value);
-    console.log(45);
     promise.then(function (response) {
-        console.log(response); // Success
         window.localStorage.setItem("user", JSON.stringify({
-            $id: response.$id,
-            userID: response.userId,
+            $id: response.userId,
             email: response.providerUid,
         }))
 
