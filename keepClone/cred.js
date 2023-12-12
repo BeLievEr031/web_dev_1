0a9891f884dcc9f574c5f405c8a7ccf87b5f057a
BeLieVer031 <sandyrajak031@gmail.com>
2023-12-12 18:05:34 +0530

    cred updated

--- keepClone/cred.js
+++ keepClone/cred.js
@@ -1,6 +1,6 @@
 let isUser = window.localStorage.getItem("user")
 let user = JSON.parse(isUser)
 
-if (!user || !user.email || !user.userID) {
+if (!user || !user.email || !user.$id) {
     window.location.replace("./login");
 }
